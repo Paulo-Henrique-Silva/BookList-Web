a48800fba6622c8363717fc207f4e08a44515dbf
Paulo <pshenrique2015@gmail.com>
2022-12-20 18:18:13 -0300

    Mensagens de erros customizadas.

--- BookList/Models/Livro.cs
+++ BookList/Models/Livro.cs
@@ -13,18 +13,21 @@ namespace BookList.Models
         public int Id { get; set; }
 
         [Column("isbn")]
-        [Required]
-        [MaxLength(13)]
+        [DisplayName("ISBN")]
+        [Required(ErrorMessage = "O campo de 'ISBN' é obrigatório.")]
+        [MaxLength(13, ErrorMessage = "O campo de ISBN só pode conter no máximo 13 caracteres.")]
         public string? Isbn { get; set; }
 
         [Column("titulo")]
-        [Required]
-        [MaxLength(80)]
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "O campo de 'Título' é obrigatório.")]
+        [MaxLength(80, ErrorMessage = "O campo de 'Título' só pode conter no máximo 80 caracteres.")]
         public string? Titulo { get; set; }
 
         [Column("autor")]
-        [Required]
-        [MaxLength(80)]
+        [DisplayName("Autor")]
+        [Required(ErrorMessage = "O campo de 'Autor' é obrigatório.")]
+        [MaxLength(80, ErrorMessage = "O campo de 'Autor' só pode conter no máximo 80 caracteres.")]
         public string? Autor { get; set; }
     }
 }
